a14e8b4d0890dde9f2b5cfdf824b74be37cf4f7d
Raiven Rovic Leandicho <rovicleandicho@gmail.com>
2026-02-06 11:55:58 +0800

    Update: resume update

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ export default function HomePage() {
                   </button>
                   
                   <a
-                    href="/RaivenL_CV.pdf"
+                    href="/RRL_CV.pdf"
                     download
                     className="px-8 py-3 rounded-md border border-white text-white font-bold hover:bg-white hover:text-black transition-all active:scale-95"
                   >
